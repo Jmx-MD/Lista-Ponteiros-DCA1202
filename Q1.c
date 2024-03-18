268f433a6f585d89184b4502ec159abe3a0cbad6
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-03-18 07:38:10 -0300

    Update Q1.c - Add justificativas questões

--- Q1.c
+++ Q1.c
@@ -12,19 +12,24 @@ int main(void){
     bol = true;
   }
   printf(bol);
-  //
+  //Imprime "true", pois p é igual ao endereço de i (&i).
     
   //*p - *q;
   a = *p - *q;
   printf(a);
+  //**Imprime o resultado da diferença do endereços contido em p e q, pois as variáves em si (ponteiros) não contém os conteúdos de i e j (números inteiro).
   
   //**&p;
   b = **&p;
   print(b);
+  //Imprime o endereço de p, pois o símbolo "&" indica endereço, enquanto os dois "*" indicam ponteiros (b, ponteiro de p, e p, ponteiro de i).
   
   //3 - *p/(*q) + 7;
   c = 3 - *p/(*q) + 7;
   printf(c);
+  //**Imprime o resultado da equação, sendo *p e *q os endereços d i e j, pois aqui se acessa as variáveis p e q em si, e não o conteúdo das variáveis às quais apontam.
+
+  //** = rever
 
   
 
