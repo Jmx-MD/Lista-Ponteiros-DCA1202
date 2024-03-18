e6863ada6322c8efa6402a6d899a15c75b021561
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-03-18 07:25:22 -0300

    Update Q1.c - prints

--- Q1.c
+++ Q1.c
@@ -1,5 +1,27 @@
 #include <std.io>
 
 int main(void){
+  int i=3,j=5;
+  int *p, *q;
+  p = &i;
+  q = &j;
+  
+  //p == &i;
+  printf(p);
+    
+  //*p - *q;
+  a = *p - *q;
+  printf(a);
+  
+  //**&p;
+  b = **&p;
+  print(b);
+  
+  //3 - *p/(*q) + 7;
+  c = 3 - *p/(*q) + 7;
+  printf(c);
+
+  
+
 
 }
