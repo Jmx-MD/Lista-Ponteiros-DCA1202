453349ca0e61a365cb9fc6314a16838e928fad18
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-03-25 18:13:01 -0300

    Update Q2.c - justifcativas

--- Q2.c
+++ Q2.c
@@ -3,6 +3,10 @@ int main(void){
   p = &i;
   printf("%p %p %d %d %d %d\n", p, p+1, *p+2, **&p, 3**p, **&p+4);
 }
-/* Será impresso: 4094 4095 7 
+/* Será impresso: 4094 4095 7 15 9
 
+Pois 
+p = endereço de i (4094)
+*p = conteúdo da variável à qual p aponta (i=5)
+**&p = *p 
 */
