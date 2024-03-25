53480853923aeb8a3c01c8b887c3af9ddcef28eb
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-03-25 18:25:04 -0300

    Update Q3.c - justificativas

--- Q3.c
+++ Q3.c
@@ -4,5 +4,10 @@ i = (*p)++ + *q;
 
 Essas afirmações estão incorretas.
 
+q = *p; 
+Pois q se trata de um endereço, e *p se trata do conteúdo da variável à qual p aponta.
+
+i = (*p)++ + *q;
+Pois não é o mesmo que "(*p)++; i= *p + *q;", onde é acrescido ao conteúdo *p e então ao conteúdo *q. Executar a linha anterior ocasionará um erro de sintaxe.
 
 */
