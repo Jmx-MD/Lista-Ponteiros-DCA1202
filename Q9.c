35e0bf0feb32015001ed293a7bc3631310d9a69c
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-03-30 13:39:07 -0300

    Create Q9.c - enunciado

---
+++ Q9.c
@@ -0,0 +1,14 @@
+/* 9. Qual será a saída do seguinte programa
+
+
+#include <stdio.h>
+struct teste{
+  int x = 3;
+  char nome[] = "jose";
+};
+main(){
+  struct teste *s;
+  printf("%d", s->x);
+  printf("%s", s->name);
+}
+*/
