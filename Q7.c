357b0711dc99b789c112c0372df62ac407785217
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-01 08:10:51 -0300

    Update Q7.c - finish

--- Q7.c
+++ Q7.c
@@ -1,8 +1,9 @@
 /* Considerando a declaração
 int mat[4], *p, x;
 quais das seguintes expressões são válidas? Justifique.
-p = mat + 1;
-p = mat;
+p = mat + 1; - Essa é valida, pois "mat" é o endereço das variáveis contidas nesse vetor, então "mat + 1" será o endereço do
+segundo item dessa lista.
+p = mat; - Esta também pe válida, pois o ponteiro "p" pode apontar para o primeiro item da lista "mat".
 p = mat;
 x = (*mat);
 */
