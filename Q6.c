25a00eaba42e00d2ec0de4ff63b908e1b3b730fa
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-01 07:45:56 -0300

    Update Q6.c - finish

--- Q6.c
+++ Q6.c
@@ -1,6 +1,10 @@
 /* Assumindo que pulo[] é um vetor do tipo int, quais das seguintes expressões referenciam o
 valor do terceiro elemento do vetor?
-◦ *(pulo + 2);
+
+◦ *(pulo + 2); - Esta expressão. Pois o valor do terceiro elemento se localiza em pulo[2], visto que se conta a partir do zero. E o operador 
+"*" derreferencia o ponteiro, revelando o conteúdo da variável à qual aponta. Neste caso, o vetor "pulo" age como um ponteiro, e seus itens
+seriam as variáveis à qual aponta; pois essa lista contém os endereços dessas variáveis.
+
 ◦ *(pulo + 4);
 ◦ pulo + 4;
 ◦ pulo + 2;
