ee1df009045114b82909bf6eb0476fdf74a27cd5
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-01 07:41:12 -0300

    Update Q5.c - finish

--- Q5.c
+++ Q5.c
@@ -1,6 +1,11 @@
 /* Determine o que será mostrado pelo seguinte programa (compile-o, execute-o e explique se
 foram obtidas as respostas esperadas). */
-  
+
+/*
+Após compilar, houveram avisos em relação aos 2 penúltimos prints (linhas 19 e 20): 
+%X se refere a um número inteiro, mas os itens da lista "vet" são floats.
+*/
+
 int main(void){
   float vet[5] = {1.1,2.2,3.3,4.4,5.5};
   float *f;
@@ -16,3 +21,8 @@ int main(void){
   printf("\n");
   }
 }
+
+/*
+Foram impressos os valores esperados, visto que floats ocupam 4 bytes na memória, e esse foi o espaçamento entre os endereços das
+variáveis na lista "vet".
+*/
