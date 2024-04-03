ad213e8b1e75f7305f4e820e7a5d2831b4640af7
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-03 09:58:19 -0300

    Update Q10.c - finish

--- Q10.c
+++ Q10.c
@@ -4,4 +4,14 @@ void main(){
   int const *x = 3;
   printf("%d", ++(*x));
 }
+
+Código alterado:
+
+#include <stdio.h>
+void main(){
+  int const *x = 3;
+  printf("%d", (*x)+1);
+}
+
+//Imprimirá o número 4.
 */
