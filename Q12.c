60381d5e02ac6191008d557e8ffd82714ec9e4ef
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-03 22:07:42 -0300

    Update Q12.c - finish

--- Q12.c
+++ Q12.c
@@ -1,16 +1,29 @@
 /*
 Suponha que as seguintes declarações tenham sido realizadas:
-5
+
 float aloha[10], coisas[10][5], *pf, value = 2.2;
 int i=3;
+
 Identifique quais dos seguintes comandos é válido ou inválido:
-aloha[2] = value;
-scanf("%f", &aloha);
-aloha = "value";
-printf("%f", aloha);
-coisas[4][4] = aloha[3];
-coisas[5] = aloha;
-pf = value;
-pf = aloha;
+aloha[2] = value; - Inválido (não foi estabelecido valor para aloha[2], logo foi impresso 0)
+scanf("%f", &aloha); - Válido (imprime o endereço do primeiro item da lista "aloha")
+aloha = "value"; - Válido
+printf("%f", aloha); - Válido (imprime o primeiro item da lista)
+coisas[4][4] = aloha[3]; - Válido (nenhum dos valores foi estabelecido, logo ambos serão 0)
+coisas[5] = aloha; - Válido (foram impressos os mesmos valores)
+pf = value; - Inválido (pf é o endereço de aloha, não seu conteúdo)
+pf = aloha; - Válido (aloha é o ponteiro de seus itens).
 
 */
+
+#include <stdio.h>
+
+int main()
+{
+    float aloha[10], coisas[10][5], *pf, value = 2.2;
+    int i=3;
+    
+    printf("%f %f %p %f %d",aloha,coisas,pf,value,i);
+    
+    return 0;
+}
