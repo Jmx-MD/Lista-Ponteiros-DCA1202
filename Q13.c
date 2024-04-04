b1d9a2dd8d20e03f61877b83cb8b195bb4b94262
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-03 22:32:46 -0300

    Update Q13.c - explanation + 3 examp

--- Q13.c
+++ Q13.c
@@ -1,4 +1,35 @@
 /*
 O que é memory leak? Procure 3 exemplos de programas em C que apresentem memory leak e
 explique o que acontece em cada um deles.
+
+Memory leak ocorre quando não é disponibilizado espaço suficiente para determinado conteúdo; não é liberado espaço antes da alocação de variáveis.
 */
+
+//exemplo 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int ptr = (int)malloc(sizeof(int));
+    ptr = (int*)malloc(sizeof(int));
+
+return 0;
+}
+
+//exemplo 2
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{   
+    int a = 12, b = 24; 
+    {
+        int *sum = (int*)malloc(sizeof(int));
+        *sum = a + b;
+    }
+   
+    printf("%d\n", *sum);
+    return 0;
+}
+
+//exemplo 3
