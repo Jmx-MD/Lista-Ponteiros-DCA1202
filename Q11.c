83341e6660acc4e390a159a6453d2ddc9fa93834
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-03 21:48:55 -0300

    Update Q11.c - finish

--- Q11.c
+++ Q11.c
@@ -15,5 +15,47 @@ valores de x+1, x+2 e x+3 se:
 Implemente um programa de computador para testar estas suposições e compare as respostas
 oferecidas pelo programa com as respostas que você idealizou.
 */
+#include <stdio.h>
 
+int main()
+{
+    char xc[4]; //x se TIPO = char
+    int xi[4]; //x se TIPO = int
+    float xf[4]; //x se TIPO = float
+    double xd[4]; //x se TIPO = double
+    
+    char *c;
+    c= &xc[0];
+    int *i;
+    i = &xi[0];
+    float *f;
+    f=&xf[0];
+    double *d;
+    d = &xd[0];
+    
+    int a;
+    
+    printf("Endereços para char x[4]:\n");//char
+    for(a=0;a<4;a++){ //imprime endereços x[0] a x[3]
+        printf("%d ",&xc[a]);
+    }
+    
+    printf("\n\nEndereços para int x[4]:\n");//int
+    //pula duas linhas por organização
+    for(a=0;a<4;a++){
+        printf("%d ",&xi[a]);
+    }
+    
+    printf("\n\nEndereços para float x[4]:\n");//float
+    for(a=0;a<4;a++){
+        printf("%d ",&xf[a]);
+    }
+    
+    printf("\n\nEndereços para double x[4]:\n");//double
+    for(a=0;a<4;a++){
+        printf("%d ",&xd[a]);
+    }
+
+    return 0;
+}
 
