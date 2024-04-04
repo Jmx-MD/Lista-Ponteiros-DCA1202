a8e4e8dbdc6610293240e4592ca65304f28dfb8e
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-03 23:43:10 -0300

    Update Q15.c - rascunho

--- Q15.c
+++ Q15.c
@@ -3,3 +3,35 @@ Implemente em linguagem C uma função em um programa de computador que leia n v
 tipo float e os apresente em ordem crescente. Utilize alocação dinâmica de memória para
 realizar a tarefa.
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordem(){
+    int i,n,a; //contador, tamanho da lista, variavel temporaria
+    scanf("%i",&n);
+    
+    float l[n];
+    
+    float *size = malloc(n);
+    
+    
+    for(i=0;i<n;i++){
+        scanf("%f",&l[i]);
+    }
+    
+    for(i=0;i<n;i++){
+        if(l[i]>l[i+1]){
+            a = l[i+1];
+            l[i+1] = l[i];
+            l[i] =a;
+        }
+        printf("%f ",l[i]);
+    }
+    
+}
+
+int main() {
+    ordem();
+
+    return 0;
+}
