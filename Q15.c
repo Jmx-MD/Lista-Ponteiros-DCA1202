19150fa1275bc0f6fa3e80a2c15cb0f94c7b7476
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-05 21:09:25 -0300

    Update Q15.c - tentativa 1

--- Q15.c
+++ Q15.c
@@ -4,34 +4,41 @@ tipo float e os apresente em ordem crescente. Utilize alocação dinâmica de me
 realizar a tarefa.
 */
 #include <stdio.h>
-#include <stdlib.h>
-
-void ordem(){
-    int i,n,a; //contador, tamanho da lista, variavel temporaria
-    scanf("%i",&n);
-    
-    float l[n];
-    
-    float *size = malloc(n);
-    
+#include <malloc.h>
+float ordem(int n)//função que recebe e ordena floats
+{
+    int i,*temp; //contador e ponteiro para mudança de posições na lista
+    float l[n],*ptr; //lista e ponteiro da lista
     
     for(i=0;i<n;i++){
         scanf("%f",&l[i]);
+        printf("%f\n",l[i]);
     }
+    
     
     for(i=0;i<n;i++){
         if(l[i]>l[i+1]){
-            a = l[i+1];
+            *temp = l[i+1];
             l[i+1] = l[i];
-            l[i] =a;
+            l[i] = *temp;
         }
+    }
+    for(i=0;i<n;i++){
         printf("%f ",l[i]);
     }
     
+    return *ptr;
+
 }
 
-int main() {
-    ordem();
-
+int main()
+{
+    int x;
+    scanf("%i",&x);
+    
+    float (*ptrl)(int) = &ordem;
+    
+    ptrl(x);
+    
     return 0;
 }
