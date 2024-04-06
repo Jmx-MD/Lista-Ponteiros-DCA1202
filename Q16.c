7963928853a6c72a8569fc3176a4e334e231db7e
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-06 19:26:00 -0300

    Update Q16.c - finish*

--- Q16.c
+++ Q16.c
@@ -3,49 +3,53 @@ Reimplemente o programa da questão anterior utilizando a função qsort() do C.
 código, explicando o que faz cada uma das linhas.
 */
 #include <stdio.h> //inclui biblioteca padrão
-#include <stdlib.h> // inclui biblioteca padrão q contém a função qsort()
-float ordem(int n)//função que recebe e ordena floats
+#include <stdlib.h> //inclui biblioteca padrão que contém a função qsort()
+int comp(const void *a,const void *b)//função que recebe e ordena inteiros, recebe dois ponteiros para itens da lista
 {
-    int i,*temp; //contador e ponteiro para mudança de posições na lista
-    float l[n],*ptr; //lista e ponteiro da lista
-    
-    for(i=0;i<n;i++){ //recebe valores contidos na lista
-        scanf("%f",&l[i]);
-    }
-    
-    
-    for(i=0;i<n;i++){//analisa valores da lista completa
+    int i;//contador 
+    int n1 = *(int *)a; //obtém valor do item ao qual *a aponta
+    int n2 = *(int *)b; //obtém valor do item ao qual *b aponta
 
-    if (l[i] < l[i+1]) //se o número anteriormente for menor
+
+    if (n1 < n2) //se o número anteriormente for menor
     {
         return -1;
     }
-    else if (l[i] > l[i+1])//se o número localizado anteriormente for maior que o posterior
+    else if (n1 > n2)//se o número localizado anteriormente for maior que o posterior
     {
         return 1;
     }
     else 
     {
-        for(i=0;i<n;i++){
-        printf("%f ",l[i]); //imprime a lista final, em ordem
-    }
         return 0;
         
     }
-    
-    
-    }
-
 }
 
 int main()
 {
-    int x;
-    scanf("%i",&x); //pede um número (tamanho da lista)
+    int x,i,*l[x]; //tamanho da lista, contador, lista com x elementos 
     
-    float (*ptrl)(int) = &ordem; //ponteiro para a função ordem
+    l[x] = (int *)malloc(x * sizeof(int)); //libera espaço para itens da lista
     
-    ptrl(x); //executa a função
+    printf("Tamanho da lista: ");
+    
+    scanf("%i",&x); //recebe um número (tamanho da lista)
+    
+    printf("\nInsira itens da lista:\n");
+    
+    for(i=0;i<x;i++){
+        scanf("%p",&l[i]);//recebe itens da lista
+    }
+ 
+    qsort(l,x,sizeof(int),comp); //ordena itens da lista
+    
+    
+    printf("Lista final:\n");
+    for(i=0;i<x;i++){
+        printf("%p ",l[i]); //imprime itens da lista final
+    }
+    
     
     return 0;
 }
