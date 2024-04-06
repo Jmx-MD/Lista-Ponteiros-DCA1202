8d9feab883e960a0ef0e04610e21a03d1e6cb407
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-05 22:33:36 -0300

    Update Q19.c - finish

--- Q19.c
+++ Q19.c
@@ -7,3 +7,42 @@ função deve receber 4 argumentos: os nomes dos três vetores e o número de el
 presentes em cada vetor. Exemplo:
 soma_vetores(vet1, vet2, resultado, 4);
 */
+
+#include <stdio.h>
+
+int *soma(int n,int a[n],int b[n],int c[n]){
+    int i;//contador
+    
+    for(i=0;i<n;i++){
+        c[i] = a[i] + b[i]; //soma elementos de a e b, pondo o resultado na mesma localização em c
+    }
+    return c;
+}
+
+int main()
+{
+    int x,i; //tamanho do vetor e contador
+    int a[x], b[x], c[x];//vetores a serem utilizados
+    
+    printf("Tamanho dos vetores: ");
+    scanf("%i",&x); //recebe tamanho dos vetores a serem somados, pelo usuário
+    
+    for(i=0;i<x;i++){ //pede para o usuário fornecer elementos do vetor
+        printf("Elemento %i do primeiro vetor:",i+1);
+        scanf("%i",&a[i]);
+        printf("Elemento %i do segundo vetor:",i+1);
+        scanf("%i",&b[i]);
+    }
+    
+    int *s = soma(x,a,b,c); //soma os vetores fornecidos
+    
+    printf("O vetor resultante da soma é: ( ");
+    for(i=0;i<x;i++){
+        printf("%i ",s[i]); //imprime valores no vetor resultante
+    }
+    printf(")");
+    
+    
+    return 0;
+}
+
