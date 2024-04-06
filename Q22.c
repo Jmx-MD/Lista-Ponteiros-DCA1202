90c79313071e0b08bd4d522ae445eea9e50dd85f
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-05 23:20:11 -0300

    Update Q22.c - finish

--- Q22.c
+++ Q22.c
@@ -7,6 +7,7 @@ como dos operadores tilizados para realizar operações binárias nas linguagens
 é essencial para o desenvolvimento desse tipo de sistema. A partir dessas informações, observe
 o código apresentado a seguir, escrito na linguagem C, que faz uso de operações binárias sobre
 variáveis inteiras.
+
 #include <stdio.h>
 int main(){
   int a, b;
@@ -23,7 +24,41 @@ int main(){
   printf ("%d %d %d\n", x, y, z);
   return 0;
 }
+
 Após a chamada desse programa, caso o usuário entre com os valores 10 e 1, nessa ordem, qual
 será, exatamente, o valor da saída do programa?
 
+A saída será "15 13 0".
 */
+//Programa alterado para teste:
+
+#include <stdio.h>
+int main(){
+  int a, b;
+  /*
+  Para valores propostos:
+  a = 10 (1010)
+  b = 1 (0001)
+  */
+  
+  int x, y, z;
+  scanf("%d %d", &a, &b);
+  x = a; y = b; z = a + b;
+  while (a) {
+  x = x | b; //1011 (11) nas primeiras 2 , 1111 (15) nas últimas 2
+  printf("%i ",x);
+  y = y ^ a; //1011 (11), 1110 (14), 1100 (12), 1101 (13)
+  printf("%i ",y);
+  z = z & (a+b); //1011 (11), 0011 (3), 0010 (2), 0000 (0)
+  printf("%i ",z);
+  a = a >> 1; //0101 (5), 0101 (5), 0001 (1), 0000 (0)
+  printf("%i ",a);
+  b = b << 1; //0010 (2), 0100 (4), 1000 (8), 10000 (16)
+  printf("%i\n",b);
+  
+  }
+ 
+  printf ("\n%d %d %d\n", x, y, z);
+  return 0;
+}
+
