11ab757a58e8117619f6402306e799d0540c2906
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-06 22:37:39 -0300

    Update Q25.c - finish*

--- Q25.c
+++ Q25.c
@@ -19,8 +19,9 @@ c. O usuário do programa de testes poderá modificar o estado de um dos element
 Insira no seu código comentários para indicar como as posições da matriz poderão ser
 acessadas.
 */
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //inclui biblioteca padrão
+#include <stdlib.h> //inclui biblioteca padrão
+#include <malloc.h> //inclui biblioteca com função malloc()
 
 int main()
 {
@@ -40,6 +41,7 @@ int main()
     
     }
     
+    printf("Digite os itens da matriz como se estivessem em uma linha só.\n");
     for(i=0;i<x;i++){
         for(j=0;j<y;j++){
             for(w=0;w<z;w++){
@@ -48,69 +50,38 @@ int main()
         }
     }
     
-    char imp1,imp2,imp3; //dimensões caso o usuário deseje imprimir algum plano
-    int r1,r2,r3;//valores para acessar plano
-    char resp1;//resposta do usuário à pergunta seguinte
-    
-    printf("Deseja imprimir algum plano? (Digite 's' para 'sim' e 'n' para 'não')\n");
-    scanf("\n%s",&resp1);
-    
-    if(resp1=='s'){
-        printf("Digite o plano que deseja imprimir (x y 0,0 y z, ou x 0 z).\n");
-        scanf("\n%s",&imp1);
-        scanf("\n%s",&imp2);
-        scanf("\n%s",&imp3);
+    /* Para imprimir um plano, utilize uma das funções abaixo (remova do comentário e execute o 
+        programa normalmente).
         
-        //adquire valores a serem considerados na impressão
-        if(imp1!='0'){
-            r1=x;
-            if(imp2!='0'){//plano xy
-                r2=y;
-                r3=0;
-                
-                for(i=0;i<r1;i++){ //imprime plano 
-                    for(j=0;j<r2;j++){
-                        printf("%p",m[i][j]);
-                        printf("\n"); //pular a cada linha finalizada
+        //Plano xy
+            for(i=0;i<x;i++){
+            for(j=0;j<y;j++){
+                printf("%i",m[i][j][0]);
             }
-            
-        }
-            }else{//plano xz
-                r2=0;
-                r3=z;
-                
-                for(i=0;i<r1;i++){ //imprime plano 
-                    for(j=0;j<r3;j++){
-                        printf("%p",m[i][j]);
-                        printf("\n"); //pular a cada linha finalizada
+            printf("\n");
             }
-                
-            }
-        }
-            
-        }else{//plano yz
-            r1=0;
-            r2=y;
-            r3=z;
-            
-            for(i=0;i<r2;i++){ //imprime plano 
-                    for(j=0;j<r3;j++){
-                        printf("%p",m[i][j]);
-                        printf("\n"); //pular a cada linha finalizada
-            }
-            
-        }}
-    }
-        
-        
     
-    if(resp1=='n'){
-        printf("\nOk!");
-    }
-    if(resp1 !='s' && resp1 !='n'){
-        printf("Por favor, digite uma resposta válida.\n");
-        scanf("%s",&resp1);
-    }
+    
+        //Plano xz
+            for(i=0;i<x;i++){
+            for(w=0;w<z;w++){
+                printf("%i",m[i][0][w]);
+            }
+            printf("\n");
+            }
+    
+    
+        //Plano yz
+            for(j=0;j<y;j++){
+            for(w=0;w<z;w++){
+                printf("%i",m[0][j][w]);
+            }
+            printf("\n");
+            }
+    
+    */
+    
+    
     
     /*para alterar algum valor da matriz, utilizar o seguinte formato:
         
@@ -123,7 +94,7 @@ int main()
     
     */
     
-    free(m);
+    free(m); //librera espaço utilizado pela matriz após uso
     
     return 0;
 }
