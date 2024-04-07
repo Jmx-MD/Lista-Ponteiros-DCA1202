a507cd1c2fef2407c58b1bb74053806bca56c413
Jmx-MD <69326613+Jmx-MD@users.noreply.github.com>
2024-04-06 21:03:15 -0300

    Update Q24.c - finish*

--- Q24.c
+++ Q24.c
@@ -1,7 +1,7 @@
 /*
 Um usuário precisa implementar o controle de uma matriz de leds com 8 x 8 elementos. Para
 isso, ele criou um programa em C dotado de uma matriz da forma
-9
+
 unsigned char m[8][8];
 para armazenar os estados dos leds. Como existem apenas dois estados possíveis para os leds
 (aceso ou apagado), ele assumiu que leds acessos seriam denotados pelo inteiro "1" nessa matriz
@@ -15,3 +15,39 @@ previsto na matriz m seja codificado em um bit correspondente na variável envia
 Crie um programa em linguagem C para realizar essa codificação e explique na forma de
 comentários como sua codificação da matriz m na variável de 64 bits foi realizada.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h> 
+
+int main()
+{
+    unsigned char m[8][8]; //matriz de leds
+    char est[8]; //lista de estados das linhas
+    int i,j,cl; //contadores
+    
+    
+    printf("Digite se os leds estão acesos (1) ou apagados (0).\n");
+    for(i=0;i<8;i++){
+        for(j=0;j<8;j++){
+            scanf("%s",&m[i][j]); //recebe valores dos leds e os põe na matriz em forma de string
+        }
+    }
+    unsigned long e; //estado da linha
+    for(i=0;i<8;i++){
+        cl=0; //após primeiro ciclo, remove dados do contador da linha anterior
+        for(j=0;j<8;j++){
+            if(m[i][j]=='1'){
+                cl++; //contador de leds ligados na linha
+            }
+        }
+        if(cl>3){
+            est[i]='1'; //se mais da metade da linha estiver ligada, o estado da linha é ligada (1)
+        }else{
+            est[i]='0'; //caso contrário, está desligada (0)
+        }
+    }
+    send(*m[8],est,1,MSG_OOB); //envia dados dos leds
+    
+
+    return 0;
+}
